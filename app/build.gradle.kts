c902eab976daab358c462cc38a5db4d28308a37d
kotleni <yavarenikya@gmail.com>
2023-11-08 18:29:57 +0200

    updated app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "app.kotleni.tiktokautoswipe"
         minSdk = 24
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     buildTypes {
